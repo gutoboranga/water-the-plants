af3d3d9796bebdf4e4120710a004d01843cccff5
Augusto Boranga <augusto.boranga@thoughtworks.com>
2021-04-02 20:26:23 -0300

    add project.yml and initial files

---
+++ WaterThePlants/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterThePlants
+//
+//  Created by Augusto Boranga on 02/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterThePlants/WaterThePlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterThePlantsApp.swift
+//  WaterThePlants
+//
+//  Created by Augusto Boranga on 02/04/21.
+//
+
+import SwiftUI
+
+@main
+struct WaterThePlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
